0dfd9fb45aaa6baa6b357d131c4489a47b14793f
Aelso <49383092+Aelso@users.noreply.github.com>
2022-12-25 08:12:45 -0300

    Udpate do projeto

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ import "./style/main.css";
 import { GiShoppingBag } from "react-icons/gi";
 import RatingStars from "./components/RatingStars";
 import ShoppingCart from "./components/ShoppingCart";
+ 
 
 const products = [
   {
@@ -210,55 +211,5 @@ function App() {
     </div>
   );
 }
-/* <footer>
-  <div className="footer-container">
-    <div className="footer-left">
-      <h3>Logo</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Voluptates, quae.
-      </p>
-      <div className="social-media">
-        <a href="#">
-          <i className="fab fa-facebook-f"></i>
-        </a>
-        <a href="#">
-          <i className="fab fa-twitter"></i>
-        </a>
-        <a href="#">
-          <i className="fab fa-instagram"></i>
-        </a>
-      </div>
-    </div>
-    <div className="footer-center">
-      <h3>Useful Links</h3>
-      <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-    <div className="footer-right">
-      <h3>Newsletter</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
-        quae.
-      </p>
-      <form action="">
-        <input type="email" placeholder="Enter your email" />
-        <button type="submit">Subscribe</button>
-      </form>
-    </div>
-  </div>
-</footer> */
 
 export default App;
